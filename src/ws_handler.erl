f81217b28f5f1c3d40552334632678251ead07c1
itsmethomas <itsmethomas1225@hotmail.com>
2015-05-20 09:08:09 +0100

    test

--- src/ws_handler.erl
+++ src/ws_handler.erl
@@ -609,6 +609,7 @@ try_register(Type, LUser, PN, DeviceId, State, MsgId) ->
 send_reg_sms(Type, _, <<>>, _, MsgId) -> make_response(Type, invalid_syntax, MsgId);
 
 send_reg_sms(Type, LUser, _PN, Resp, MsgId) ->
+	?INFO_MSG("Process got something unexpected ~p ~n",[Type, LUser, _PN, Resp, MsgId]),
     mnesia:dirty_write(reg_tokens, #reg_tokens{user = LUser, token = <<"1234">>}),
     make_response(Type, Resp, MsgId).
 
