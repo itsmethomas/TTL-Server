7c6d647d50022a55e072d65c8edd7101bcf7f5bc
itsmethomas <itsmethomas1225@hotmail.com>
2015-05-21 23:08:21 +0100

    test

--- src/mod_push.erl
+++ src/mod_push.erl
@@ -23,7 +23,7 @@ ex_apns:start(apple, ws_config:get_local_option(ios_profile),
                 {ws_config:get_local_option(ios_push_cert), ws_config:get_local_option(ios_cert_pass)}),
 
 %%     ex_apns:start(apple, ws_config:get_local_option(ios_profile), 
-                  ws_config:get_local_option(ios_push_cert)),
+%%                   ws_config:get_local_option(ios_push_cert)),
   nexmo_push:start(),
   register(apple_token_cleaner, spawn(?MODULE, feedback, [undefined])),
   mnesia:create_table(user_token, [{disc_copies, [node()]},
