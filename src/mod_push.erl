cb45ad4b99ebce7096e4fd1b43846ab9da136bbe
itsmethomas <itsmethomas1225@hotmail.com>
2015-05-22 07:15:07 +0100

    test

--- src/mod_push.erl
+++ src/mod_push.erl
@@ -105,6 +105,7 @@ push(User, Msg) ->
 
 push_apple(Token, Msg) when (Token /= <<>>) ->
     Payload = jsx:encode([{<<"aps">>, [{<<"alert">>, Msg}, {<<"badge">>, 0}, {<<"sound">>, <<"default">>}]}]),
+	?INFO_MSG ("Payload : ~p ~n", [Payload]),
     ex_apns:send(apple, Token, Payload).
 
 get_timestamp_for_device_token() ->
